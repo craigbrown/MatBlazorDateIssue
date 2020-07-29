3a19a0754a9156b8305633af791283bd740c0c52
Craig Brown <craigbrown24@gmail.com>
2020-07-29 17:56:15 +0000

    Added test pages for MatDatePicker

--- MatBlazorDateIssue/Startup.cs
+++ MatBlazorDateIssue/Startup.cs
@@ -29,6 +29,8 @@ namespace MatBlazorDateIssue
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+
+            services.AddScoped<TimeZoneService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ MatBlazorDateIssue/wwwroot/js/timezone.js
@@ -0,0 +1,4 @@
+﻿// Used to get the user's timezone
+function blazorGetTimezoneOffset() {
+    return new Date().getTimezoneOffset();
+}
