015b2f3c0f92993d2bed1368f99f6aaa103c16f6
Craig Brown <8934235+craigbrown@users.noreply.github.com>
2020-07-29 17:56:15 +0000

    Added test pages for MatDatePicker

---
+++ MatBlazorDateIssue/TimeZoneService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace MatBlazorDateIssue
+{
+    public sealed class TimeZoneService
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        private TimeSpan? _userOffset;
+
+        public TimeZoneService(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public async ValueTask<DateTimeOffset> GetLocalDateTime(DateTimeOffset dateTime)
+        {
+            var userOffset = await GetTimezoneOffset();
+            return dateTime.ToOffset(userOffset);
+        }
+
+        public async ValueTask<DateTime> GetLocalDateTime()
+        {
+            var userOffset = await GetTimezoneOffset();
+            return DateTime.SpecifyKind(DateTime.UtcNow.Add(userOffset), DateTimeKind.Local);
+        }
+
+        /// <summary>
+        /// Returns the time difference in minutes between this user's timezone and UTC, obtained via Javascript.
+        /// </summary>
+        private async ValueTask<TimeSpan> GetTimezoneOffset()
+        {
+            if (_userOffset == null)
+            {
+                var offsetInMinutes = await _jsRuntime.InvokeAsync<int>("blazorGetTimezoneOffset");
+                _userOffset = TimeSpan.FromMinutes(-offsetInMinutes);
+            }
+            return _userOffset.Value;
+        }
+    }
+}
